158683e00ab730dfe493febb35a3efefae73b39c
Rodolfo Dias Venancio <darth.rodolfo@gmail.com>
2020-01-30 21:57:52 -0300

    Prettier & Eslint config

--- src/routes.js
+++ src/routes.js
@@ -2,13 +2,11 @@ import { Router } from 'express';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ hello: 'Birl' })
-})
+routes.get('/', (req, res) => res.json({ hello: 'Birl' }));
 
-//routes.get();
-//routes.post();
-//routes.delete();
-//routes.put();
+// routes.get();
+// routes.post();
+// routes.delete();
+// routes.put();
 
 export default routes;
