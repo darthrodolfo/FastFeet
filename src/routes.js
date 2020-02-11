2e6f65dd49027bbe261ff21fb793e28b0f491051
Rodolfo Dias Venancio <darth.rodolfo@gmail.com>
2020-02-10 23:50:07 -0300

    Adicionando novas rotas no script de Rotas

--- src/routes.js
+++ src/routes.js
@@ -2,18 +2,23 @@ import { Router } from 'express';
 // import User from './app/models/User';
 
 import UserController from './app/controllers/UserController';
+import RecipientController from './app/controllers/RecipientController';
 import SessionController from './app/controllers/SessionController';
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
-routes.get('/users', (req, res) => res.json({ get_teste: 'test' }));
-routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware);
 
+routes.get('/recipients', RecipientController.index);
+routes.post('/recipients', RecipientController.store);
+routes.put('/recipients', RecipientController.update);
+
 routes.put('/users', UserController.update);
+routes.get('/users', UserController.index);
+routes.post('/users', UserController.store);
 
 // routes.delete();
 // routes.put();
